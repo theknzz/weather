c20e64c86ed77b24415ca58210a901845cb0f7d1
theknzz <joaquimpersonalmail@isec.pt>
2020-10-21 15:33:48 +0100

    Updates

---
+++ precache-manifest.08a4db6fe83a63002fcbbff8f09f903b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7924570719d6138ee0d956aa911e9876",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "2c47083c5f5302b12609",
+    "url": "/weather/static/css/2.d8355f35.chunk.css"
+  },
+  {
+    "revision": "007876baaa62fb612e39",
+    "url": "/weather/static/css/main.f6e27d04.chunk.css"
+  },
+  {
+    "revision": "2c47083c5f5302b12609",
+    "url": "/weather/static/js/2.d1dda587.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/weather/static/js/2.d1dda587.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "007876baaa62fb612e39",
+    "url": "/weather/static/js/main.01e2eee4.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weather/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weather/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weather/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weather/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/weather/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
